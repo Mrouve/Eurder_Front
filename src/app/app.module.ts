1ee861c292c358d3d063a1744489eb49a240b124
Mrouve <maximerouve@hotmail.com>
2023-05-04 10:47:44 +0200

    [SETUP] - LAYOUT - Restructuration - deletion of Layout module ; Header and Footer directly in App html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { LayoutModule } from './layout/layout.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
