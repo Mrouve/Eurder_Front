2e13d7b521ff7e737fe07088bd27f8e6abcfb9cf
Mrouve <maximerouve@hotmail.com>
2023-05-04 11:07:49 +0200

    [SETUP] - ITEMS - created items module ; created item-overview/create/detail component ; reference html css for item-overview ; item-overview as default route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsModule } from './items/items.module';
+import { CustomersModule } from './customers/customers.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ItemsModule,
+    CustomersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
