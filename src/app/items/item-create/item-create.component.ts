65175f3f82ca2390152a247ec380852101f2cb63
Mrouve <maximerouve@hotmail.com>
2023-05-05 08:10:50 +0200

    [ADD] - ITEM - IT3 v1 ; functional but no counter yet or misstype check

--- src/app/items/item-create/item-create.component.ts
+++ src/app/items/item-create/item-create.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {ItemService} from "../../service/item.service";
+import {FormBuilder, Validators} from "@angular/forms";
+//import {NewItemForm} from "../../../../models/NewItemForm";
 
 @Component({
   selector: 'app-item-create',
   templateUrl: './item-create.component.html',
-  styleUrls: ['./item-create.component.css']
+  styleUrls: ['../../app.component.css']
 })
 export class ItemCreateComponent {
+
+  newItemForm = this.formBuilder.group({
+    id: '',
+    name:['', Validators.required],
+    description:['', Validators.required],
+    price:['', Validators.required],
+    amountOfStock:['', Validators.required],
+  })
+
+
+  constructor(private itemService:ItemService, private formBuilder:FormBuilder){
+    this.newItemForm.reset();
+  }
+
+  onSubmit():void{
+
+    this.itemService.addItem(this.newItemForm.value).subscribe(content => {
+      this.newItemForm.reset();
+    });
+  }
 
 }
--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment.development";
 import {map, Observable} from "rxjs";
 import {ItemOverview} from "../../../models/ItemOverview";
+import {ɵElement, ɵFormGroupValue, ɵTypedOrUntyped} from "@angular/forms";
+//import {NewItemForm} from "../../../models/NewItemForm";
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +22,11 @@ export class ItemService {
         map(i=>i.sort((a:ItemOverview, b:ItemOverview) => a.name.localeCompare(b.name)))
       );
   }
+
+  addItem(newItem: ɵTypedOrUntyped<{ [K in keyof { price: string; name: string; description: string; id: string; amountInStock: string }]: ɵElement<{ price: string; name: string; description: string; id: string; amountInStock: string }[K], null> }, ɵFormGroupValue<{ [K in keyof { price: string; name: string; description: string; id: string; amountInStock: string }]: ɵElement<{ price: string; name: string; description: string; id: string; amountInStock: string }[K], null> }>, any>):Observable<any>{
+    return this.http.post(this.varUrl, newItem);
+  }
+
+
+
 }
