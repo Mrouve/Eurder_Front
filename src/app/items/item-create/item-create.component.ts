dc09985f6998fe11912044691594e8b1179c6318
Mrouve <maximerouve@hotmail.com>
2023-05-05 10:38:55 +0200

    [IMP] - ITEM - IT4 / added char counters to create item description ; blocked at 0 + red ; validator added in itemCreateComponent;

--- src/app/items/item-create/item-create.component.ts
+++ src/app/items/item-create/item-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {ItemService} from "../../service/item.service";
 import {FormBuilder, Validators} from "@angular/forms";
 //import {NewItemForm} from "../../../../models/NewItemForm";
@@ -13,21 +13,44 @@ export class ItemCreateComponent {
   newItemForm = this.formBuilder.group({
     id: '',
     name:['', Validators.required],
-    description:['', Validators.required],
+    description:['', Validators.required, Validators.maxLength(255)],
     price:['', Validators.required],
     amountOfStock:['', Validators.required],
   })
 
+  maxlength:number = 255;
+  counter:string = ``;
+  textAreaValue:string = '';
+  characterCount:number = 0;
 
   constructor(private itemService:ItemService, private formBuilder:FormBuilder){
     this.newItemForm.reset();
   }
 
   onSubmit():void{
-
     this.itemService.addItem(this.newItemForm.value).subscribe(content => {
       this.newItemForm.reset();
     });
   }
+
+  public ngOnInit(): void {
+    this.onValueChange();
+  }
+
+  public onValueChange(): void{
+    this.characterCount =  255-(this.newItemForm.get('description')?.value?.length || 0);
+    this.checkIfNegative(this.characterCount);
+    this.counter = `${this.characterCount}/${this.maxlength}`;
+
+
+  }
+
+  public checkIfNegative(remainingCharacters:number): void{
+    if(remainingCharacters <0){
+      this.characterCount = 0;
+    }
+  }
+
+
 
 }
